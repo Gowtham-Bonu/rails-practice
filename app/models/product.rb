e6a4542ce5a33dd58e00b819f06465d03598ae63
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-01-27 19:04:15 +0530

    TE7-T116:Active-Record-Basics:Feat:active-record-basics-application

---
+++ app/models/product.rb
@@ -0,0 +1,19 @@
+class Product < ApplicationRecord
+    validates :name, presence: true
+    before_validation :name_is_not_null
+
+    after_initialize do |product|
+        puts "product is initialized"
+    end
+
+    after_create do |product|
+        puts "record is successfully created"
+    end
+
+    private
+
+    def name_is_not_null
+        puts "name must have a value"
+    end
+
+end
